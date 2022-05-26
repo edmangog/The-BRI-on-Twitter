2b1b9e484100ed589ffa242560d82dea0b96d4b9
edmangog <cycourban@gmail.com>
2022-05-26 15:02:45 +0800

    scraper.py

--- 1.scraper.py
+++ 1.scraper.py
@@ -1,56 +1,96 @@
-# RUN with PYTHON 3.8+
-# SNSCRAPE VERSION: 0.3.5.dev138+ga6b6f3f (commit:a6b6f3f)
-import snscrape.modules.twitter as SMT
+import tweepy
 import pandas as pd
+from rich import print
+import json
 
-query = SMT.TwitterSearchScraper(
-    '"belt and road" OR "beltandroad" OR "one belt one road" OR "onebeltoneroad" OR "new silk road" OR "newsilkroad" OR "silk road economic belt" OR "silkroadeconomicbelt" OR "maritime silk road" OR "maritimesilkroad" since:2013-09-01 until:2020-09-01').get_items()
+API = tweepy.Client(
+    bearer_token='',
+    return_type=dict
+)
 
-tweet_detail = []
+def get_tweet(query):
+    return API.search_all_tweets(
+        query=query,
+        start_time='2021-11-18T10:00:00Z',
+        end_time='2021-11-19T00:00:00Z',
+        expansions=[
+            'author_id',
+            'referenced_tweets.id',
+            'referenced_tweets.id.author_id',
+            'entities.mentions.username',
+            'attachments.poll_ids',
+            'attachments.media_keys',
+            'in_reply_to_user_id',
+            'geo.place_id'
+        ],
+        media_fields=[
+            'duration_ms',
+            'height',
+            'media_key',
+            'preview_image_url',
+            'type',
+            'url',
+            'width',
+            'public_metrics',
+            'alt_text'
+        ],
+        place_fields=[
+            'contained_within',
+            'country',
+            'country_code',
+            'full_name',
+            'geo',
+            'id',
+            'name',
+            'place_type'
+        ],
+        poll_fields=[
+            'duration_minutes',
+            'end_datetime',
+            'id',
+            'options',
+            'voting_status',
+        ],
+        tweet_fields=[
+            'attachments',
+            'author_id',
+            'context_annotations',
+            'conversation_id',
+            'created_at',
+            'entities',
+            'geo',
+            'id',
+            'in_reply_to_user_id',
+            'lang',
+            # 'non_public_metrics',
+            # 'organic_metrics',
+            'possibly_sensitive',
+            # 'promoted_metrics',
+            'public_metrics',
+            'referenced_tweets',
+            'reply_settings',
+            'source',
+            'text',
+            'withheld'],
+        user_fields=[
+            'created_at',
+            'description',
+            'entities',
+            'id',
+            'location',
+            'name',
+            'pinned_tweet_id',
+            'profile_image_url',
+            'protected',
+            'public_metrics',
+            'url',
+            'username',
+            'verified',
+            'withheld'
+        ],
+    )
 
-for i, tweet in enumerate(query):
-    print(i)
-    tweet_detail.append({
-        # snscrape.base.Item
-                        "screen_name": tweet.user.username,
-                        "username":  tweet.user.displayname,
-                        "tweet_url": tweet.url,
-                        "timestamp": tweet.date,
-                        "tweet_id": tweet.id,
-                        "text": tweet.content,
-                        "text_lang": tweet.lang,
-                        "links": tweet.outlinks,
-                        "hashtag": tweet.hashtags,
-                        "cashtag": tweet.cashtags,
-                        "media": tweet.media,
-                        "likes": tweet.likeCount,
-                        "retweets": tweet.retweetCount,
-                        "replies": tweet.replyCount,
-                        "reply_to_user": tweet.inReplyToUser,
-                        "reply_to_user_tweet": tweet.inReplyToTweetId,
-                        "mentioned_users": tweet.mentionedUsers,
-                        "quoted_tweet": tweet.quotedTweet,
-                        "quoted_by_count": tweet.quoteCount,
-                        # "original_tweet": tweet.retweetedTweet,
-                        "coordinates": tweet.coordinates,
-                        "place": tweet.place,
-                        "tweet_source": tweet.sourceLabel,
-		# snscrape.base.Entity
-                        "user_id": tweet.user.id,
-                        "bio": tweet.user.description,
-                        "verified": tweet.user.verified,
-                        "label": tweet.user.label,
-                        "date_joined": tweet.user.created,
-                        "profile_url": tweet.user.linkUrl,
-                        "profile_image": tweet.user.profileImageUrl,
-                        "profile_banner": tweet.user.profileBannerUrl,
-                        "profile_location": tweet.user.location,
-                        # "protected": tweet.user.protected,
-                        "num_tweets": tweet.user.statusesCount,
-                        "media_count": tweet.user.mediaCount,
-                        "followers": tweet.user.followersCount,
-                        "following": tweet.user.friendsCount,
-                        })
+result = get_tweet("belt and road OR beltandroad OR one belt one road OR onebeltoneroad OR new silk road OR newsilkroad OR silk road economic belt OR silkroadeconomicbelt OR maritime silk road OR maritimesilkroad")
+df = pd.json_normalize(result)
+df.to_csv('test.csv', encoding='utf-8-sig')
 
-df = pd.DataFrame(tweet_detail)
-df.to_csv('result.csv', encoding='utf-8-sig')
